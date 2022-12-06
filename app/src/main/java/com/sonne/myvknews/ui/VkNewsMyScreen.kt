9b7bd19e42b2ae0d014b3abc6f451acee3474c2f
sonne <sonnne34@gmil.com>
2022-12-06 17:46:48 +0500

    use floatingActionButton, snackBar.

--- app/src/main/java/com/sonne/myvknews/ui/VkNewsMyScreen.kt
+++ app/src/main/java/com/sonne/myvknews/ui/VkNewsMyScreen.kt
@@ -1,16 +1,47 @@
 package com.sonne.myvknews.ui
 
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
 @Preview
 @Composable
 fun MyScreen() {
+    val snackBarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        val action = snackBarHostState.showSnackbar(
+                            message = "I Like it",
+                            actionLabel = "Hide",
+                            duration = SnackbarDuration.Long
+                        )
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabIsVisible.value = false
+                        }
+                    }
+                }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        },
         bottomBar = {
             BottomNavigation {
                 val selectedItemPosition = remember {
