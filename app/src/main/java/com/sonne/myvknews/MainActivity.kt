4df61e3f95625f0fae31636cca492c21e2ff5d41
sonne <sonnne34@gmil.com>
2022-12-06 17:21:07 +0500

    use Scaffold: bottomBar.

--- app/src/main/java/com/sonne/myvknews/MainActivity.kt
+++ app/src/main/java/com/sonne/myvknews/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.sonne.myvknews.ui.CardPost
+import com.sonne.myvknews.ui.MyScreen
 import com.sonne.myvknews.ui.theme.MyVkNewsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
                         .background(MaterialTheme.colors.background)
                         .padding(8.dp)
                 ) {
-                    CardPost()
+                    MyScreen()
                 }
             }
         }
