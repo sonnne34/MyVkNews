cf234816e89790adb72dff577da0d93bf03b6b16
sonne <sonnne34@gmil.com>
2022-12-09 14:50:59 +0500

    add onClick for Statistics

--- app/src/main/java/com/sonne/myvknews/ui/VkNewsMyScreen.kt
+++ app/src/main/java/com/sonne/myvknews/ui/VkNewsMyScreen.kt
@@ -9,10 +9,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sonne.myvknews.domain.FeedPost
 
 @Preview
 @Composable
 fun MyScreen() {
+
+    val feedPost = remember {
+        mutableStateOf(FeedPost())
+    }
+
     Scaffold(
         bottomBar = {
             BottomNavigation {
@@ -43,7 +49,21 @@ fun MyScreen() {
         }
     ) {
         CardPost(
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp),
+            feedPost = feedPost.value,
+            onStatisticsClickListener = { newItem ->
+                val oldStatistics = feedPost.value.statistics
+                val newStatistics = oldStatistics.toMutableList().apply {
+                    replaceAll { oldItem ->
+                        if (oldItem.type == newItem.type) {
+                            oldItem.copy(count = oldItem.count + 1)
+                        } else {
+                            oldItem
+                        }
+                    }
+                }
+                feedPost.value = feedPost.value.copy(statistics = newStatistics)
+            }
         )
     }
 }
