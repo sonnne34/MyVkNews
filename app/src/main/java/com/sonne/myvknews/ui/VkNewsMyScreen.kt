64d406ef98345617dfc4abd6aad3b038bec11b77
sonne <sonnne34@gmil.com>
2022-12-13 16:52:04 +0500

    use popUpTo, saveState, launchSingleTop

--- app/src/main/java/com/sonne/myvknews/ui/VkNewsMyScreen.kt
+++ app/src/main/java/com/sonne/myvknews/ui/VkNewsMyScreen.kt
@@ -9,6 +9,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.sonne.myvknews.MainViewModel
 import com.sonne.myvknews.domain.navigation.AppNavGraph
+import com.sonne.myvknews.domain.navigation.Screen
 
 @Composable
 fun MyScreen(viewModel: MainViewModel) {
@@ -28,7 +29,13 @@ fun MyScreen(viewModel: MainViewModel) {
                 items.forEach { item ->
                     BottomNavigationItem(
                         selected = currentRout == item.screen.route,
-                        onClick = { navHostController.navigate(item.screen.route) },
+                        onClick = { navHostController.navigate(item.screen.route){
+                            popUpTo(Screen.NewsFeed.route){
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        } },
                         icon = {
                             Icon(item.icon, contentDescription = null)
                         },
