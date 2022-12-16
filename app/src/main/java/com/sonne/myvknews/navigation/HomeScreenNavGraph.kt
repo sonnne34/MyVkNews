a06de9040a9045fd08f0b4e03614c082f34880bf
sonne <sonnne34@gmil.com>
2022-12-16 16:08:22 +0500

    fix bug args type

--- app/src/main/java/com/sonne/myvknews/navigation/HomeScreenNavGraph.kt
+++ app/src/main/java/com/sonne/myvknews/navigation/HomeScreenNavGraph.kt
@@ -2,7 +2,9 @@ package com.sonne.myvknews.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.sonne.myvknews.domain.FeedPost
 
@@ -17,7 +19,14 @@ fun NavGraphBuilder.homeScreenNavGraph(
         composable(Screen.NewsFeed.route) {
             feedNewsScreenContent()
         }
-        composable(Screen.Comments.route) {
+        composable(
+            route = Screen.Comments.route,
+            arguments = listOf(
+                navArgument(Screen.KEY_FEED_POST_ID) {
+                    type = NavType.IntType
+                }
+            )
+        ) {
             val feedPostId = it.arguments?.getInt(Screen.KEY_FEED_POST_ID) ?: 0
             commentsScreenContent(FeedPost(id = feedPostId))
         }
