413bd81f799685bafe2d8734675e86017ff0e8ca
sonne <sonnne34@gmil.com>
2022-12-20 14:46:02 +0500

    receiving token

--- app/src/main/java/com/sonne/myvknews/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/sonne/myvknews/presentation/main/MainViewModel.kt
@@ -1,18 +1,23 @@
 package com.sonne.myvknews.presentation.main
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKPreferencesKeyValueStorage
+import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthenticationResult
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _authState = MutableLiveData<AuthState>(AuthState.Initial)
     val authState: LiveData<AuthState> = _authState
 
     init {
-        _authState.value = if (VK.isLoggedIn()) AuthState.Authorised else AuthState.NotAuthorised
+        val storage = VKPreferencesKeyValueStorage(application)
+        val token = VKAccessToken.restore(storage)
+        val loggedIn = token != null && token.isValid
+        _authState.value = if (loggedIn) AuthState.Authorised else AuthState.NotAuthorised
     }
 
     fun performAuthResult(result: VKAuthenticationResult) {
